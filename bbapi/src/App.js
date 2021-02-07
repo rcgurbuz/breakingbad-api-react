2b73c94871e551e45e2394ce060c8b6738317858
rcgurbuz <rifatcgurbuz@gmail.com>
2021-02-07 12:40:27 +0300

    first commit

---
+++ bbapi/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './component/Header';
+import CharacterGrid from './component/CharacterGrid';
+import Search from './component/Search';
+import './App.css';
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      );
+
+      setItems(result.data);
+      setIsLoading(false);
+    };
+    fetchItems();
+  }, [query]);
+
+  return (
+    <div className='App'>
+      <Header />
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} items={items} />
+    </div>
+  );
+}
+
+export default App;
---
+++ bbapi/src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../src/img/logo.png';
+
+const Header = () => {
+  return (
+    <div className='center'>
+      <img src={logo} alt='' />
+    </div>
+  );
+};
+
+export default Header;
